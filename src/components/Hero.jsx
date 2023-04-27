ca756a10dc9483c4b1b810f95a8d743e099aae9b
erickkipkoech <cheruiyotkipkoech612@gmail.com>
2023-04-27 16:06:20 +0300

    completed the computer section and the scroll section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-[7xl] mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[30px] max-w-[7xl] mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -23,6 +23,13 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute sm:bottom-20 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="rounded-3xl w-[35px] h-[64px] border-4 border-secondary justify-center flex items-start p-2">
+            <motion.dev animate={{ y: [0, 24, 0] }} transition={{duration:1.5,repeat:Infinity,repeatType:'loop'}} className="bg-secondary w-3 h-3 rounded-full bg-secondary mb-1" />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,32 +3,53 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoarder from "../Loader";
 
-const Computers = () => {
+const Computers = ({isMobile}) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} scale={0.75} position={[0,-3.25,-1.5]} rotation={[-0.01,-0.2,-0.1]} />
+      <spotLight position={[-20,50,10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024}/>
+      <primitive object={computer.scene} scale={isMobile ? 0.7:0.75} position={isMobile ? [0,-3,-2.2]:[0,-3.25,-1.5]} rotation={[-0.01,-0.2,-0.1]} />
     </mesh>
   );
 };
 
 const ComputersCanvas = () => {
+  const [isMobile,setIsMobile]=useState(false);
+
+  useEffect(()=>{
+    //event listener that changes to screen size
+    const mediaQuery=window.matchMedia('(max-width:500px)');
+    //set the initial value of isMobile state variable
+    setIsMobile(mediaQuery.matches);
+    //define callback to handle changes to media query
+    const handleMediaQueryChange=(event)=>{
+      setIsMobile(event.matches)
+    }
+
+    mediaQuery.addEventListener('change',handleMediaQueryChange);
+
+    return()=>{
+      //remove listener when componnt is unmounted
+      mediaQuery.removeEventListener('change',handleMediaQueryChange)
+    }
+  },[])
+
   return (
     <Canvas
-      frameLoop="demand"
+      frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense>
+      <Suspense fallback={<CanvasLoarder/>}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile}/>
       </Suspense>
 
       <Preload all />
