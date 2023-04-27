bb418ec00dccca9fe08d8049a70a407df247aa11
erickkipkoech <cheruiyotkipkoech612@gmail.com>
2023-04-27 20:29:51 +0300

    about section created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className="absolute sm:bottom-20 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute sm:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="rounded-3xl w-[35px] h-[64px] border-4 border-secondary justify-center flex items-start p-2">
             <motion.dev animate={{ y: [0, 24, 0] }} transition={{duration:1.5,repeat:Infinity,repeatType:'loop'}} className="bg-secondary w-3 h-3 rounded-full bg-secondary mb-1" />
